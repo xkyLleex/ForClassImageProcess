34430eef88fc0ab159ca26e778248259912c263a
kyL_lee <leekwanyao710@gmail.com>
2022-03-16 15:43:54 +0800

    Add Jupyter Background Module

--- Environment.bat
+++ Environment.bat
@@ -1,8 +1,8 @@
 @echo off
-
+echo Init Environment
 python -m pip install --upgrade pip
 pip install ipython[notebook]
-explorer C:\Users\User
-ipython notebook
-
+pip install jupyterthemes
+jt -t chesterish
+echo Finish
 pause
