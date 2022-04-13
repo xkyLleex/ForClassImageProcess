495b37801b941498d1e180844e2a81c3325ad68e
kyL_lee <leekwanyao710@gmail.com>
2022-04-13 12:51:12 +0800

    Update Environment.bat

--- Environment.bat
+++ Environment.bat
@@ -2,6 +2,8 @@
 echo Init Environment
 python -m pip install --upgrade pip
 pip install ipython[notebook]
+pip install –v opencv-python==4.2.0.34
+pip install pillow
 pip install jupyterthemes
 jt -t chesterish
 echo Finish
